7d531d786aae71f27a41d520b16593202c205bc2
pakpoom.s@transcodeglobal.com <pakpoom.s@transcodeglobal.com>
2024-07-30 15:56:23 +0700

    update

--- ICPT2024/settings.py
+++ ICPT2024/settings.py
@@ -119,8 +119,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'deletezak@gmail.com'
-EMAIL_HOST_PASSWORD = 'mtkuechdpwwkxhsk'
+EMAIL_HOST_USER = 'icpt2025.info@gmail.com'
+EMAIL_HOST_PASSWORD = 'twtkpgcezsaimvgn'
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
