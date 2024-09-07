1116aec77a0387dbca39e78c8128ed09d95f9de5
kawank.kh <kawank.kh@outlook.com>
2024-09-07 12:51:47 +0700

    navigate

--- ICPT2024/settings.py
+++ ICPT2024/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#@76&r-)1$z2lbj9xlco*&9_^l9rq*i)(_ud(9%rh27@oaa12b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
